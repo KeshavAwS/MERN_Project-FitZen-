b1edd3717159a7b2037e21a9cb661f19c54ed3b3
KeshavS <keshav6987@email.com>
2024-05-04 18:35:44 +0530

    indexEdit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,13 +8,13 @@ export const UserSignUp = async (data) => API.post("/user/signup", data);
 export const UserSignIn = async (data) => API.post("/user/signin", data);
 
 export const getDashboardDetails = async (token) => API.get("/user/dashboard", {
-    headers: {Authorization: `Bearer ${token}`},
+    headers: { Authorization: `Bearer ${token}` },
 });
 
-export const getWorkouts = async (token,date) => API.get(`/user/workout${date}`, {
-    headers: {Authorization: `Bearer ${token}`},
+export const getWorkouts = async (token, date) => API.get(`/user/workout${date}`, {
+    headers: { Authorization: `Bearer ${token}` },
 });
 
-export const addWorkout = async (token, data) => API.get("/user/workout", data, {
-    headers: {Authorization: `Bearer ${token}`},
+export const addWorkout = async (token, data) => API.post("/user/workout", data, {
+    headers: { Authorization: `Bearer ${token}` },
 });
