ab70b0810dbf041533353049b3787b5d75aa5101
KeshavS <keshav6987@email.com>
2024-05-04 17:39:19 +0530

    envEdit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://mern-project-fitzen.onrender.com",
+    baseURL: "https://mern-project-fitzen.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
